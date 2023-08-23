0c0758754d74c56366f0202880f0b44930a6c10b
unknown <youssefhoussein701@gmail.com>
2023-08-23 22:02:55 +0300

    frontend <> fetch the getUsers

--- frontend/src/components/sideBar/index.jsx
+++ frontend/src/components/sideBar/index.jsx
@@ -1,9 +1,28 @@
-
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './style.css'
 import {AiOutlinePlus} from "react-icons/ai"
 import UserBar from '../userBar'
 const SideBar = ({openProfile,openCreate}) => {
-  
+  const [users_names,setUserName] = useState([])
+  useEffect(()=>{
+    const getUsers = async () => {
+      try{
+          const config = {
+            headers: {
+                'Authorization': `Bearer ${localStorage.getItem("token")}`,
+            }
+        };
+          const response = await axios.post('http://127.0.0.1:8000/users/getAllUsers',null,config)
+          setUserName(response.data)
+      }
+      catch(err){
+        console.log(err)
+      }
+      
+    }
+    getUsers()
+  })
   return (
     <div className='sideBar flex column center spaceEvenly'>
       <div className='flex center'>
@@ -11,10 +30,12 @@ const SideBar = ({openProfile,openCreate}) => {
         <AiOutlinePlus className='pointer' onClick={openCreate}/>
       </div>
       <div className="users flex column">
-        <div className='header-section'>Users</div>
-        <div className="user-bar-sidebar flex center">
-          <UserBar name={"Youssef"} following={false}/>
-        </div>
+        {
+        users_names.map((user) => (
+            <div key={user._id} className="user-bar-sidebar flex center">
+              <UserBar name={user.name} following={false} />
+            </div>
+          ))}
       </div>
       <div className="followings flex column">
         <div className='header-section'>Following</div>
