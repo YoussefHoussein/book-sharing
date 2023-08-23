051e989ce0e8d16cc7ef6a162a43e3bc24001530
unknown <youssefhoussein701@gmail.com>
2023-08-23 15:13:34 +0300

    frontend <> add the create book design

--- frontend/src/components/create/index.jsx
+++ frontend/src/components/create/index.jsx
@@ -1,8 +1,77 @@
-import React from 'react'
-
+import React, { useRef, useState } from 'react'
+import './style.css'
 const Create = () => {
+    const [data ,setData] = useState({
+        name : "",
+        author : "",
+        review : "",
+    })
+    const [error ,SetError] = useState("")
+    const [selectedImage, setSelectedImage] = useState(null);
+    const fileInputRef = useRef(null);
+    const imageContainerRef = useRef(null);
+
+    const handleImageClick = () =>{
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    }
+
+    const handleChange = (e) =>{
+        setData({ ...data, [e.target.name]: e.target.value });
+    }
+
+    const handleImageChange = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = (e) => {
+            setSelectedImage(e.target.result);
+            
+            if (imageContainerRef.current) {
+              imageContainerRef.current.style.backgroundImage = `url(${e.target.result})`;
+            }
+          };
+          reader.readAsDataURL(file);
+        }
+      };
+    const checkData = () => {
+        const name = data.name
+        const author = data.author
+        const review = data.review
+        if(!name || !author || !review || ! selectedImage){
+            SetError("Please fill all fields")
+            setData({
+                name:"",
+                author: "",
+                review: ""
+            })
+        }
+        else{
+            handleInsert()
+        }
+    }
+    const handleInsert = () => {
+
+    }
   return (
-    <div>Create</div>
+    <div className='create-container flex column spaceEvenly'>
+        {error ? <h2 className='create-warning'>{error}</h2> : <h2 className='create-title'>Share new Book</h2>}
+        
+        <div className="image-input-container flex center">
+            <input type="file" name="" className='input-image' ref={fileInputRef} onChange={handleImageChange}/>
+            <div className="image flex center pointer" ref={imageContainerRef} onClick={handleImageClick}>
+                {selectedImage ?  "" : "Upload Image"}
+                
+            </div>
+        </div>
+        <div className="create-input-container flex column spaceBetween">
+            <input type="text" name='name' value={data.name} className="create-input create-input-text" placeholder='Name' onChange={handleChange}/>
+            <input type="text" name='author' value={data.author} className="create-input create-input-text" placeholder='Author' onChange={handleChange}/>
+            <input type="text" name='review' value={data.review} className="create-input create-input-text" placeholder='Short Review' onChange={handleChange}/>
+            <button className='create-input create-button' onClick={checkData}>Share</button>
+        </div>
+    </div>
   )
 }
 
