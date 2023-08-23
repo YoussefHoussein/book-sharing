12ca568c178afdf179323b8453e378e5f9b1b869
unknown <youssefhoussein701@gmail.com>
2023-08-23 14:27:51 +0300

    frontend <> add the open profile and create

--- frontend/src/components/home/index.jsx
+++ frontend/src/components/home/index.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../navbar'
 import SideBar from '../sideBar'
 import './style.css'
+import Profile from '../profile'
+import Create from '../create'
 const Home = () => {
+  const [active,setActive]=useState(null)
   return (
-    <div className='flex'>
+    <div className='flex column'>
         <Navbar name={"name"}/>
-        <div className='sidebar-container flex'>
-        <SideBar />
+        <div className='body-container flex spaceBetween'>
+          <div className='sidebar-container flex'>
+          <SideBar openProfile={() => setActive("profile")} openCreate={()=>setActive("create")}/>
+          </div>
+          <div className='profile-container flex center'>
+            {active === "profile" ? <Profile /> : ""}
+            {active === "create" ? <Create /> : ""}
+          </div>
+          
         </div>
-        
     </div>
   )
 }
--- frontend/src/components/sideBar/index.jsx
+++ frontend/src/components/sideBar/index.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './style.css'
-const SideBar = () => {
+import {AiOutlinePlus} from "react-icons/ai"
+const SideBar = ({openProfile,openCreate}) => {
   return (
     <div className='sideBar flex center'>
-      <div>Your Profile</div>
+      <div className='flex center'>
+        <button className='profile-button' onClick={openProfile}>Your Profile</button>
+        <AiOutlinePlus className='pointer' onClick={openCreate}/>
+      </div>
     </div>
   )
 }
