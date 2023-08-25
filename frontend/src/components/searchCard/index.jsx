9b088802b9bc87f794bcc2695d4ff6672596ae6c
unknown <youssefhoussein701@gmail.com>
2023-08-25 03:32:09 +0300

    frontend <> add to the search function

--- frontend/src/components/searchCard/index.jsx
+++ frontend/src/components/searchCard/index.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './style.css'
 const SearchCard = ({name,author,picture}) => {
-  return (
-    <div>
 
+    const pictureHolder = useRef(null);
+    if (pictureHolder.current) {
+        pictureHolder.current.style.backgroundImage = `url(${picture})`;
+      }
+  return (
+    <div className='flex center spaceEvenly'>
+        <div className="search-card-picture" ref={pictureHolder}></div>
+        <div className="search-card-name">{name}</div>
+        <div className="search-card-author">{author}</div>
     </div>
   )
 }
